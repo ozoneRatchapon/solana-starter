7bfc936a80f7fe587b72cec9cb2d88711e5e7964
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-01-16 23:36:18 +0700

    RUG Day: create nft image, mint, and metadata

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,11 +1,17 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import wallet from "../wba-wallet.json";
+
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
+const image_path = "./generug.png";
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
@@ -14,17 +20,19 @@ umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        //1. Load image
-        //2. Convert image to generic file.
-        //3. Upload image
-
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+  try {
+    //1. Load image
+    const image = await readFile(image_path);
+    //2. Convert image to generic file.
+    const generic_file = createGenericFile(image, "generug", {
+      displayName: "Rug",
+      contentType: "image/png",
+    });
+    //3. Upload image
+    const [myUri] = await umi.uploader.upload([generic_file]);
+    console.log("Your image URI: ", myUri);
+    // https://devnet.irys.xyz/6AsELgdAUcoC58KiCENPWVcP9AbBDFg1C8eaCGLDJpzV
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -1,8 +1,16 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
-import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  generateSigner,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import {
+  createNft,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "../wba-wallet.json";
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -11,16 +19,24 @@ const umi = createUmi(RPC_ENDPOINT);
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+  let tx = createNft(umi, {
+    mint: mint,
+    sellerFeeBasisPoints: percentAmount(50),
+    name: "GreenRug",
+    uri: "https://devnet.irys.xyz/Hh2HWh3ntyFr89sHxeuG2z36N8d9PekQdEMAX2nQwgQU",
+  });
+  let result = await tx.sendAndConfirm(umi);
+  const signature = base58.encode(result.signature);
 
-    console.log("Mint Address: ", mint.publicKey);
+  console.log(
+    `Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`,
+  );
+  // https://explorer.solana.com/tx/5z8Gnhn6JP2GD2PS27zaJfNGjPiBkc14Daq7F1MjobDEcxzCkVa3K9Y58A7hRhsVAQEbQTnMTdkV1tzm9t9AGWBG?cluster=devnet
+  console.log("Mint Address: ", mint.publicKey);
+  // 6ttRsXC72Ghzu2WUuULyr32Pcc6gD891RQQSkGWmJhUj
 })();
